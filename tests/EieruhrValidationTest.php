7b36fdfd160d8add885de3832f03db8900684343
Till Brede <tb@symcon.de>
2020-02-05 11:30:59 +0100

    added test submodule nad validation test

---
+++ tests/EieruhrValidationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . '/stubs/Validator.php';
+class EieruhrValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateEieruhr(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+    public function testValidateEieruhrModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../Eieruhr');
+    }
+}
